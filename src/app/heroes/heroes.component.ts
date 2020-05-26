00c5fca55f8cda99e4ab4dffa8bf6d18e71cbd91
dyzzbao <1095238893@qq.com>
2020-05-26 15:28:58 +0800

    使用heroService服务

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,17 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
+// import { MessageService } from '../message.service';
+import { from } from 'rxjs';
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css'],
 })
 export class HeroesComponent implements OnInit {
-  heroes = HEROES;
+  heroes: Hero[];
   selectedHero: Hero;
-  constructor() {}
-  ngOnInit(): void {}
+  constructor(
+    // private messageService: MessageService
+    private heroService: HeroService
+  ) {}
+  ngOnInit(): void {
+    this.getHeroes();
+  }
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+  }
+
+  getHeroes(): void {
+    this.heroes = this.heroService.getHeroes();
   }
 }
